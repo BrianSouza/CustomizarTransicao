0b02560be84f0f0b9696a5e9880b8e35bd6ec884
Brian Diego de Souza <Brian-desouza@hotmail.com>
2019-04-16 23:31:28 -0300

    Add project files.

---
+++ CustomizarTransicao/CustomizarTransicao/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FormsControls.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustomizarTransicao
+{
+    public partial class MainPage : ContentPage, IAnimationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public IPageAnimation PageAnimation { get; } = new SlidePageAnimation { Duration = AnimationDuration.Long, Subtype = AnimationSubtype.FromTop };
+        public void OnAnimationFinished(bool isPopAnimation)
+        {
+        }
+
+        public void OnAnimationStarted(bool isPopAnimation)
+        {
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+    }
+}
---
+++ CustomizarTransicao/CustomizarTransicao/Page1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using FormsControls.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CustomizarTransicao
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage, IAnimationPage
+    {
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        public IPageAnimation PageAnimation { get; } = new SlidePageAnimation { Duration = AnimationDuration.Long, Subtype = AnimationSubtype.FromTop };
+
+
+        public void OnAnimationFinished(bool isPopAnimation)
+        {
+        }
+
+        public void OnAnimationStarted(bool isPopAnimation)
+        {
+        }
+    }
+}
